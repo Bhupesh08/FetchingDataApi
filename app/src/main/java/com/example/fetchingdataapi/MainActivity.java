8afa89468f2195b3dc657c66b4d3a70e74e2fdb2
Bhupesh <bhupeshbansal08@gmail.com>
2024-03-06 19:10:08 +0530

    successfully wrote the header

--- app/src/main/java/com/example/fetchingdataapi/MainActivity.java
+++ app/src/main/java/com/example/fetchingdataapi/MainActivity.java
@@ -18,6 +18,8 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -44,10 +46,15 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void parseJSON() {
-        String url = "https://dummyjson.com/products";
 
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url,
+    private void parseJSON() {
+
+        String url = "https://www.purplle.com/api/shop/cart?version=v2&tstamp=0.06464658925667677";
+        String accessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkZXZpY2VfaWQiOiIxME5xMEM3ZWxxRDhjMXFNRnYiLCJtb2RlX2RldmljZSI6ImRlc2t0b3AiLCJtb2RlX2RldmljZV90eXBlIjoid2ViIiwiaWF0IjoxNzA5NjU0OTA3LCJleHAiOjE3MTc0MzA5MDcsImF1ZCI6IndlYiIsImlzcyI6InRva2VubWljcm9zZXJ2aWNlIn0.-3mDWSd73DSEhM2S1aakIFlmYohpXAuNe6srpsgT2Hk";
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                Request.Method.GET,
+                url,
                 null, // JSON object request payload
                 new Response.Listener<JSONObject>() {
                     @Override
@@ -55,14 +62,16 @@ public class MainActivity extends AppCompatActivity {
                         // Handle successful response
                         // Process the JSON response here
                         try {
-                            JSONArray jsonArray = response.getJSONArray("products");
+                            JSONArray jsonArray = response.getJSONArray("cartItems");
 
                             for(int i=0; i<jsonArray.length(); i++) {
                                 JSONObject jsonObject = jsonArray.getJSONObject(i);
 
-                                String imageurl = jsonObject.getString("thumbnail");
-                                String name = jsonObject.getString("title");
-                                int price = jsonObject.getInt("price");
+                                JSONObject imageobject = jsonObject.getJSONObject("images");
+                                String imageurl = imageobject.getString("smallImage");
+
+                                String name = jsonObject.getString("itemName");
+                                int price = jsonObject.getInt("itemId");
 
                                 list.add(new ModalClass(imageurl, name, price));
 
@@ -82,7 +91,28 @@ public class MainActivity extends AppCompatActivity {
                         // Handle error response
                         // Handle errors such as network errors, server errors, etc.
                     }
-                });
+                }
+        ) {
+            @Override
+            public Map<String, String> getHeaders() {
+                Map<String, String> headers = new HashMap<>();
+//                headers.put("Accept", "application/json, text/plain, */*");
+//                headers.put("Content-Type", "application/x-www-form-urlencoded");
+//                headers.put("Is_ssr", "false");
+//                headers.put("Mode_device", "mobile");
+//                headers.put("Referer", "https://www.purplle.com/");
+                headers.put("Token", accessToken);
+//                headers.put("User-Agent", "Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1");
+                headers.put("Visitorppl", "10Nq0C7elqD8c1qMFv");
+                return headers;
+            }
+
+        };
+
         requestQueue.add(jsonObjectRequest);
     }
 }
+
+
+
+
